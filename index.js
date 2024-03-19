3b576c94a4d6bc44a4d35de33a19d2f45b061c16
Stephen Onyango <onyangosteve01@gmail.com>
2024-03-20 00:34:31 +0300

    Adds function expressions

---
+++ index.js
@@ -0,0 +1,17 @@
+
+//question 1
+const divide = function(a,b){
+    return a / b;
+}
+divide (2000,100);
+
+//squestion 2
+
+const square = x => x * x
+
+square (2);
+
+//question 3
+const add = (a,b) => a + b;
+
+add (3,4);
